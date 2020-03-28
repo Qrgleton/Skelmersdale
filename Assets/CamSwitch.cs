78e8fc5b230c711d4600656370d7f98d2d825733
Qrgleton <58011892+Qrgleton@users.noreply.github.com>
2020-03-28 23:19:43 +0000

    initial commit

---
+++ Assets/CamSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+
+    public GameObject cam1;
+    public GameObject cam2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Aim"))
+        {
+            cam2.SetActive(true);
+            cam1.SetActive(false);
+        }
+        
+    }
+}
---
+++ Assets/CinemachineExamples/Scenes/OverTheShoulderAim/ActivateOnKeypress.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class ActivateOnKeypress : MonoBehaviour
+{
+    public KeyCode ActivationKey = KeyCode.LeftControl;
+    public int PriorityBoostAmount = 10;
+    public GameObject Reticle;
+
+    Cinemachine.CinemachineVirtualCameraBase vcam;
+    public bool boosted = false;
+    bool m_isAxisInUse = false;
+    float lt_input;
+
+    void Start()
+    {
+        vcam = GetComponent<Cinemachine.CinemachineVirtualCameraBase>();
+    }
+
+    void Update()
+    {
+        if (vcam != null)
+        {
+            if (Input.GetKey(ActivationKey))
+            //bool v = lt_input == 0;
+            //if (v)
+            {
+                if (!boosted)
+                {
+                    vcam.Priority += PriorityBoostAmount;
+                    boosted = true;
+                }
+            }
+            else if (boosted)
+            {
+                vcam.Priority -= PriorityBoostAmount;
+                boosted = false;
+            }
+        }
+        if (Reticle != null)
+            Reticle.SetActive(boosted);
+    }
+    
+    void GetInput()
+    {
+        lt_input = Input.GetAxis("Aim");
+        Debug.Log(lt_input);
+    }
+
+}
